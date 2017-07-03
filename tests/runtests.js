ae1e9fc34677a163e923383e9f68d8ee730445dc
josh <joshua@marinacci.org>
2017-07-02 22:29:18 -0700

    added tests for quad curves

--- tests/runtests.js
+++ tests/runtests.js
@@ -241,6 +241,50 @@ test('stroke circle (arc)', (t)=>{
     t.equal(img.getPixelRGBA(50,50),BLACK);
     t.end();
 });
+test('fill quad curves', (t) => {
+    var img = PImage.make(150,150);
+
+    var ctx = img.getContext('2d');
+    ctx.fillStyle = blue;
+
+    // Quadratric curves example
+    ctx.beginPath();
+    ctx.moveTo(75,25);
+    ctx.quadraticCurveTo(25,25,25,62.5);
+    ctx.quadraticCurveTo(25,100,50,100);
+    ctx.quadraticCurveTo(50,120,30,125);
+    ctx.quadraticCurveTo(60,120,65,100);
+    ctx.quadraticCurveTo(125,100,125,62.5);
+    ctx.quadraticCurveTo(125,25,75,25);
+    ctx.fill();
+
+    t.equal(img.getPixelRGBA(80,25),BLACK);
+    t.equal(img.getPixelRGBA(80,26),BLUE);
+    t.end();
+});
+
+
+test('stroke quad curves', (t) => {
+    var img = PImage.make(150,150);
+
+    var ctx = img.getContext('2d');
+    ctx.strokeStyle = blue;
+
+    // Quadratric curves example
+    ctx.beginPath();
+    ctx.moveTo(75,25);
+    ctx.quadraticCurveTo(25,25,25,62.5);
+    ctx.quadraticCurveTo(25,100,50,100);
+    ctx.quadraticCurveTo(50,120,30,125);
+    ctx.quadraticCurveTo(60,120,65,100);
+    ctx.quadraticCurveTo(125,100,125,62.5);
+    ctx.quadraticCurveTo(125,25,75,25);
+    ctx.stroke();
+
+    t.equal(img.getPixelRGBA(80,25),BLUE);
+    t.equal(img.getPixelRGBA(50,50),BLACK);
+    t.end();
+});
 
 return;
 
