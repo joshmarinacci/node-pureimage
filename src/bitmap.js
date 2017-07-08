de1cd7a0765667c97f17d0b97051543cd7f19933
josh <joshua@marinacci.org>
2017-07-08 16:25:54 -0700

    consolidate code, saving 11 lines

--- src/bitmap.js
+++ src/bitmap.js
@@ -15,59 +15,39 @@ class Bitmap {
         }
 
     }
-    setPixelRGBA(x,y,rgba) {
+    calculateIndex (x,y) {
         x = Math.floor(x);
         y = Math.floor(y);
-        if(x < 0) return;
-        if(y < 0) return;
-        if(x >= this.width) return;
-        if(y >= this.height) return;
-        var i = (this.width * y + x)*4;
-        var bytes = uint32.getBytesBigEndian(rgba);
+        if (x<0 || y<0 || x >= this.width || y >= this.height) return 0;
+        return (this.width*y+x)*4;
+    }
+    setPixelRGBA(x,y,rgba) {
+        let i = this.calculateIndex(x, y);
+        const bytes = uint32.getBytesBigEndian(rgba);
         this.data[i+0] = bytes[0];
         this.data[i+1] = bytes[1];
         this.data[i+2] = bytes[2];
         this.data[i+3] = bytes[3];
     }
     setPixelRGBA_i(x,y,r,g,b,a) {
-        x = Math.floor(x);
-        y = Math.floor(y);
-        if(x < 0) return;
-        if(y < 0) return;
-        if(x >= this.width) return;
-        if(y >= this.height) return;
-        var i = (this.width * y + x)*4;
+        let i = this.calculateIndex(x, y);
         this.data[i+0] = r;
         this.data[i+1] = g;
         this.data[i+2] = b;
         this.data[i+3] = a;
     }
     getPixelRGBA(x,y) {
-        x = Math.floor(x);
-        y = Math.floor(y);
-        if(x<0) return 0;
-        if(y<0) return 0;
-        if(x >= this.width) return 0;
-        if(y >= this.height) return 0;
-        var i = (this.width * y + x) * 4;
+        let i = this.calculateIndex(x, y);
         return uint32.fromBytesBigEndian(
             this.data[i+0],
             this.data[i+1],
             this.data[i+2],
             this.data[i+3]);
     }
-
     getPixelRGBA_separate(x,y) {
-        x = Math.floor(x);
-        y = Math.floor(y);
-        if(x<0) return 0;
-        if(y<0) return 0;
-        if(x >= this.width) return 0;
-        if(y >= this.height) return 0;
-        var i = (this.width * y + x) * 4;
+        var i = this.calculateIndex(x,y);
         return this.data.slice(i,i+4);
     }
-
     getContext(type) {
         return new Context(this);
     }
