e15cf46baeea5bf8e9973c449d19c877dc93c80c
Robert Main <lenswipe@gmail.com>
2017-10-09 19:56:48 -0400

    Named Colors
    Add docblock annotations for the NamedColors enum
    
    Ref #32

--- src/named_colors.js
+++ src/named_colors.js
@@ -1,8 +1,12 @@
 /**
- * Created by josh on 8/21/16.
+ * @enum {string}
  */
 var NAMED_COLORS = {
-    'white':0xFFFFFFff, 'black':0x000000ff,  'red':0xFF0000ff,  'green':0x00FF00ff, 'blue':0x0000FFff,
+    'white': 0xFFFFFFff,
+    'black': 0x000000ff,
+    'red'  : 0xFF0000ff,
+    'green': 0x00FF00ff,
+    'blue' : 0x0000FFff
 }
 
 module.exports = NAMED_COLORS;
