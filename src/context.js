5312b09c49d896d73ebbea10c916dc2df8dd0c88
Robert Main <lenswipe@gmail.com>
2017-11-24 00:10:03 -0500

    MDN Documentation
    Update `Context` to more closely match the documentation on MDN, since it is intended to match the API available from the HTML5 `<canvas>` API. In each case - a link has been provided to the appropriate MDN page, both for attribution and for further reference.
    
    Ref #32

--- src/context.js
+++ src/context.js
@@ -17,9 +17,10 @@ const PATH_COMMAND = {
     QUADRATIC_CURVE: 'q',
     BEZIER_CURVE: 'b'
 };
-
-/**
- * Context
+/*
+ * Used for drawing rectangles, text, images and other objects onto the canvas element. It provides the 2D rendering context for a drawing surface.
+ *
+ * It has the same API as [CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D) from the HTML5 canvas spec
  *
  * @class Context
  */
@@ -133,7 +134,7 @@ class Context {
     };
 
     /**
-     * The thickness of lines in space units. When getting, it returns the current value (1.0 by default). When setting, zero, negative, Infinity and NaN values are ignored; otherwise the current value is set to the new value.
+     * The thickness of lines in space units. When getting, it returns the current value (1.0 by default). When setting, zero, negative, `Infinity` and `NaN` values are ignored; otherwise the current value is set to the new value.
      *
      * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineWidth
      * @type {number}
@@ -191,12 +192,11 @@ class Context {
         this._font.size   = font_size;
     }
 
+
     /**
-     * Save
+     * Saves the entire state of the canvas by pushing the current state onto a stack
      *
-     * Save the current state of the transform
-     *
-     * @see {@link Transform}
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/save
      *
      * @returns {void}
      *
@@ -207,9 +207,9 @@ class Context {
     }
 
     /**
-     * Translate
+     * Adds a translation transformation by moving the canvas and its origin `x` horizontally and `y` vertically on the grid
      *
-     * Translate the context according to the X and Y co-ordinates passed in
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/translate
      *
      * @param {number} x X position
      * @param {number} y Y position
@@ -223,11 +223,11 @@ class Context {
     }
 
     /**
-     * Rotate
+     * Add a rotation to the transformation matrix. The angle argument represents a clockwise rotation angle and is expressed in adians
      *
-     * Rorate the
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rotate
      *
-     * @param {number} angle The degrees of rotation (in radians)
+     * @param {number} angle Degrees of rotation (in radians)
      *
      * @returns {void}
      *
@@ -238,9 +238,9 @@ class Context {
     }
 
     /**
-     * Scale
+     * Adds a scaling transformation to the canvas units by `x` horizontally and by `y` vertically
      *
-     * Scale the current context by the amount given
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rotate
      *
      * @param {number} sx Scale X amount
      * @param {number} sy Scale Y amount
@@ -254,7 +254,9 @@ class Context {
     }
 
     /**
-     * Restore
+     * Restores the most recently saved canvas state by popping the top entry in the drawing state stack. If there is no saved state, this method does nothing.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/restore
      *
      * @returns {void}
      *
@@ -266,9 +268,9 @@ class Context {
 
 
     /**
-     * Fill Rect
+     * Draws a filled rectangle whose starting point is at the coordinates `(x, y)` with the specified width and height and whose style is determined by the fillStyle attribute.
      *
-     * Draw a simple rectangle
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillRect
      *
      * @param {number} x X position
      * @param {number} y Y position
@@ -288,7 +290,9 @@ class Context {
     }
 
     /**
-     * Clear Rect
+     * Sets all pixels in the rectangle defined by starting point `(x, y)` and size `(width, height)` to transparent black, erasing any previously drawn content.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clearRect
      *
      * @param {number} x X position
      * @param {number} y Y position
@@ -308,7 +312,9 @@ class Context {
     }
 
     /**
-     * Stroke Rect
+     * Paints a rectangle which has a starting point at `(x, y)` and has a `w` width and an `h` height onto the canvas, using the current stroke style.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/strokeRect
      *
      * @param {number} x X position
      * @param {number} y Y position
@@ -484,17 +490,19 @@ class Context {
     }
 
     /**
-     * Draw Image
+     * Provides different ways to draw an image onto the canvas.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
      *
      * @param {Bitmap} bitmap An instance of the {@link Bitmap} class to use for drawing
-     * @param {number} sx
-     * @param {number} sy
-     * @param {number} sw
-     * @param {number} sh
-     * @param {number} dx
-     * @param {number} dy
-     * @param {number} dw
-     * @param {number} dh
+     * @param {number} sx     The X coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+     * @param {number} sy     The Y coordinate of the top left corner of the sub-rectangle of the source image to draw into the destination context.
+     * @param {number} sw     The width of the sub-rectangle of the source {@link Bitmap} to draw into the destination context. If not specified, the entire rectangle from the coordinates specified by `sx` and `sy` to the bottom-right corner of the image is used.
+     * @param {number} sh     The height of the sub-rectangle of the source {@link Bitmap} to draw into the destination context.
+     * @param {number} dx     The X coordinate in the destination canvas at which to place the top-left corner of the source {@link Bitmap}
+     * @param {number} dy     The Y coordinate in the destination canvas at which to place the top-left corner of the source {@link Bitmap}
+     * @param {number} dw     The width to draw the {@link Bitmap} in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in width when drawn
+     * @param {number} dh     The height to draw the {@link Bitmap} in the destination canvas. This allows scaling of the drawn image. If not specified, the image is not scaled in height when drawn
      *
      * @returns {void}
      *
@@ -515,12 +523,12 @@ class Context {
 
 
     /**
-     * Begin Path
+     * Starts a new path by emptying the list of sub-paths. Call this method when you want to create a new path.
      *
-     * Initialize the `path` attribue to hold the path points
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/beginPath
      *
      * @returns {void}
-     *
+     *o
      * @memberof Context
      */
     beginPath() {
@@ -531,10 +539,12 @@ class Context {
     }
 
     /**
-     * Move the "pen" to a given point specified by `x` and `y`. API sugar for {@link _moveTo}
+     * Moves the starting point of a new sub-path to the (x, y) coordinates.
      *
-     * @param {number} x X position
-     * @param {number} y Y position
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/moveTo
+     *
+     * @param {number} x The x axis of the point.
+     * @param {number} y The y axis of the point.
      *
      * @returns {void}
      *
@@ -545,9 +555,9 @@ class Context {
     }
 
     /**
-     * Move the "pen" to a given point
+     * Moves the starting point of a new sub-path to the (x, y) coordinates.
      *
-     * @param {object} pt A `point` object representing a set of co-ordinates to move the "pen" to.
+     * @param {Point} pt A `point` object representing a set of co-ordinates to move the "pen" to.
      *
      * @example
      * //All of the following are valid:
@@ -568,10 +578,12 @@ class Context {
     }
 
     /**
-     * Line To
+     * Connects the last point in the sub-path to the x, y coordinates with a straight line (but does not actually draw it).
      *
-     * @param {number} x X position
-     * @param {number} y Y position
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
+     *
+     * @param {number} x The x axis of the coordinate for the end of the line.
+     * @param {number} y The y axis of the coordinate for the end of the line.
      *
      * @returns {void}
      *
@@ -582,7 +594,7 @@ class Context {
     }
 
     /**
-     * Line To
+     * Connects the last point in the sub-path to the x, y coordinates with a straight line (but does not actually draw it).
      *
      * @param {Point} pt A point object to draw a line to from the current set of co-ordinates
      *
@@ -595,14 +607,14 @@ class Context {
     }
 
     /**
-     * Quadratic Curve To
+     * Adds a quadratic Bézier curve to the path. It requires two points. The first point is a control point and the second one is the end point. The starting point is the last point in the current path, which can be changed using moveTo() before creating the quadratic Bézier curve.
      *
-     * Create a quadratic curve
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/quadraticCurveTo
      *
-     * @param {number} cp1x Curve point X position
-     * @param {number} cp1y Curve point Y position
-     * @param {number} x    Curve X position
-     * @param {number} y    Curve Y position
+     * @param {number} cp1x The x axis of the coordinate for the control point.
+     * @param {number} cp1y The y axis of the coordinate for the control point.
+     * @param {number} x    The x axis of the coordinate for the end point.
+     * @param {number} y    The y axis of the coordinate for the end point.
      *
      * @returns {void}
      *
@@ -610,21 +622,21 @@ class Context {
      */
     quadraticCurveTo(cp1x, cp1y, x,y) {
         let cp1 = this.transform.transformPoint(new Point(cp1x, cp1y));
-        let pt = this.transform.transformPoint(new Point(x, y));
+        let pt  = this.transform.transformPoint(new Point(x, y));
         this.path.push([PATH_COMMAND.QUADRATIC_CURVE, cp1, pt]);
     }
 
     /**
-     * Bezier Curve To
+     * Adds a cubic Bézier curve to the path. It requires three points. The first two points are control points and the third one is the end point. The starting point is the last point in the current path, which can be changed using moveTo() before creating the Bézier curve.
      *
-     * Create a bezier curve betweeen two points
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/bezierCurveTo
      *
-     * @param {number} cp1x Curve point 1 X position
-     * @param {number} cp1y Curve point 1 Y position
-     * @param {number} cp2x Curve point 2 X position
-     * @param {number} cp2y Curve point 2 Y position
-     * @param {number} x    Curve X position
-     * @param {number} y    Curve Y position
+     * @param {number} cp1x The x axis of the coordinate for the first control point.
+     * @param {number} cp1y The y axis of the coordinate for first control point.
+     * @param {number} cp2x The x axis of the coordinate for the second control point.
+     * @param {number} cp2y The y axis of the coordinate for the second control point.
+     * @param {number} x    The x axis of the coordinate for the end point.
+     * @param {number} y    The y axis of the coordinate for the end point.
      *
      * @returns {void}
      *
@@ -653,14 +665,16 @@ class Context {
     }
 
     /**
-     * Arc
+     * Adds an arc to the path which is centered at (x, y) position with radius r starting at startAngle and ending at endAngle going in the given direction by anticlockwise (defaulting to clockwise).
      *
-     * @param {number}  x         X position
-     * @param {number}  y         Y position
-     * @param {number}  rad       Arc radius
-     * @param {number}  start     Arc start
-     * @param {number}  end       Arc end
-     * @param {boolean} clockwise Set arc direction (`true` for clockwise, `false` for anti-clockwise)
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/arc
+     *
+     * @param {number}  x         The x coordinate of the arc's center
+     * @param {number}  y         The y coordinate of the arc's center
+     * @param {number}  rad       The arc's radius
+     * @param {number}  start     The angle at which the arc starts, measured clockwise from the positive x axis and expressed in radians
+     * @param {number}  end       The angle at which the arc ends, measured clockwise from the positive x axis and expressed in radians
+     * @param {boolean} clockwise A boolean which, if true, causes the arc to be drawn clockwise between the two angles.
      *
      * @returns {void}
      *
@@ -719,7 +733,9 @@ class Context {
     }
 
     /**
-     * Clip
+     * Turns the path currently being built into the current clipping path.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/clip
      *
      * @returns {void}
      *
@@ -743,7 +759,9 @@ class Context {
     }
 
     /**
-     * Close Path
+     * Causes the point of the pen to move back to the start of the current sub-path. It tries to add a straight line (but does not actually draw it) from the current point to the start. If the shape has already been closed or has only one point, this function does nothing.
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/closePath
      *
      * @returns {void}
      *
@@ -755,7 +773,9 @@ class Context {
 
 
     /**
-     * Stroke
+     * Strokes the current or given path with the current stroke style
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/stroke
      *
      * @returns {void}
      *
@@ -874,7 +894,9 @@ class Context {
     }
 
     /**
-     * Fill
+     * Fills the current or given path with the current fill style. Uses {@link fill_aa} and {@link fill_noaa} depending on the the value of {@link imageSmoothingEnabled}
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fill
      *
      * @returns {void}
      *
@@ -997,14 +1019,14 @@ class Context {
         }
     }
 
-
-
     /**
-     * Fill Text
+     *  Draws a text string at the specified coordinates, filling the string's characters with the current foreground color
      *
-     * @param {string} text The text to fill
-     * @param {number} x    X position
-     * @param {number} y    Y position
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText
+     *
+     * @param {string} text A string specifying the text string to render into the context. The text is rendered using the settings specified by {@link font}.
+     * @param {number} x    The x -coordinate of the point at which to begin drawing the text, in pixels.
+     * @param {number} y    The y-coordinate of the point at which to begin drawing the text, in pixels.
      *
      * @returns {void}
      *
@@ -1013,11 +1035,11 @@ class Context {
     fillText(text, x ,y) { TEXT.processTextPath(this, text, x,y, true);  }
 
     /**
-     * Stroke Text
+     * Draws the outlines of the characters of a specified text string at the given (x, y) position.
      *
-     * @param {string} text The text to stroke
-     * @param {number} x    X position
-     * @param {number} y    Y position
+     * @param {string} text The text to draw using the current {@link font} values.
+     * @param {number} x    The x axis of the coordinate for the text starting point.
+     * @param {number} y    The y axis of the coordinate for the text starting point.
      *
      * @returns {void}
      *
