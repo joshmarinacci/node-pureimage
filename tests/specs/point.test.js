dfc02b76c7a3ab9241dba4ff88ec33e8da3b99f9
Robert Main <lenswipe@gmail.com>
2017-11-26 22:38:04 -0500

    Point Test
    Add unit tests for `Point` object
    
    Ref #43

---
+++ tests/specs/point.test.js
@@ -0,0 +1,10 @@
+const Point = require('../../src/Point');
+
+describe('Point', () => {
+    it('represents a set of x and y co-ordinates in 2D space', () => {
+        let point = new Point(20, 60);
+
+        expect(point.x).toBe(20);
+        expect(point.y).toBe(60);
+    });
+});
