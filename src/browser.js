e5763d09874d6fa71e02823839e1bb30c015a0e1
Josh Marinacci <jmarinacci@lyft.com>
2021-02-23 11:58:14 -0800

    create a shim to work in the browser

--- src/browser.js
+++ src/browser.js
@@ -1,1 +1,9 @@
 console.log("we are in the browser. No need to do anything. Just use new Canvas()")
+
+exports.make = function(width,height) {
+    let canvas = document.createElement('canvas')
+    canvas.width = width
+    canvas.height = height
+    return canvas
+}
+
