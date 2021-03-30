f776e5990fb09635331981774448588f3be40e63
josh <joshua@marinacci.org>
2021-03-30 09:38:15 -0700

    fix umd imports

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
     input: 'src/index.js',
     output: {
-        file: 'dist/pureimage-umd.js',
+        file: 'dist/pureimage-umd.cjs',
         format: 'umd',
         name: 'PureImage'
     },
