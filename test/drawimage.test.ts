6e4f9a1337fb6b5534ca114231a8d2e823b7d0c6
Josh Marinacci <joshua@marinacci>
2023-08-12 13:50:04 -0700

    updated draw image test to typescript

--- test/drawimage.test.ts
+++ test/drawimage.test.ts
@@ -1,4 +1,4 @@
-import chai, {expect} from "chai"
+import {describe, beforeEach, it, expect} from "vitest"
 import * as pureimage from "../src/index.js"
 
 describe('drawImage',() => {
@@ -18,38 +18,33 @@ describe('drawImage',() => {
         c.fillRect(25,0,25,50)
     })
 
-    it('canvas is empty and clear', (done) => {
+    it('canvas is empty and clear', () => {
         expect(image.getPixelRGBA(0,0)).to.eq(0x000000FF)
-        done()
     })
 
-    it('can draw a full image', (done) => {
+    it('can draw a full image', () => {
         context.drawImage(src,0,0,50,50,0,0,50,50)
         expect(image.getPixelRGBA(0, 0)).to.eq(0xFFFFFFFF)
         expect(image.getPixelRGBA(25,0)).to.eq(0x000000FF)
-        done()
     })
 
-    it('can stretch a full image', (done) => {
+    it('can stretch a full image', () => {
         context.drawImage(src,0,0,50,50,0,0,200,200)
         expect(image.getPixelRGBA(0, 0)).to.eq(0xFFFFFFFF)
         expect(image.getPixelRGBA(25,0)).to.eq(0xFFFFFFFF)
         expect(image.getPixelRGBA(100,0)).to.eq(0x000000FF)
         expect(image.getPixelRGBA(199,0)).to.eq(0x000000FF)
-        done()
     })
 
-    it('can draw a plain image',(done) => {
+    it('can draw a plain image',() => {
         context.drawImage(src,0,0)
         expect(image.getPixelRGBA(0, 0)).to.eq(0xFFFFFFFF)
         expect(image.getPixelRGBA(25,0)).to.eq(0x000000FF)
-        done()
     })
 
-    it('can draw a scaled image',(done) => {
+    it('can draw a scaled image',() => {
         context.drawImage(src,0,0,200,200)
         expect(image.getPixelRGBA(0, 0)).to.eq(0xFFFFFFFF)
         expect(image.getPixelRGBA(100,0)).to.eq(0x000000FF)
-        done()
     })
 })
