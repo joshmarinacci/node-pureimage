8c657ed88cc9d10136b2756282abe48a0275313c
Josh Marinacci <joshua@marinacci>
2023-08-12 16:26:17 -0700

    more work on the build

---
+++ build.js
@@ -0,0 +1,31 @@
+import {build} from 'esbuild'
+import pkg from 'npm-dts'
+const {Generator} = pkg
+
+async function go() {
+    const sharedConfig = {
+        entryPoints: ['src/index.ts'],
+        bundle: true,
+        minify: true,
+    }
+
+    await new Generator({
+        entry: 'src/index.ts',
+        output: 'dist/index.d.ts',
+    }).generate();
+
+    await build({
+        ...sharedConfig,
+        platform: 'node',
+        outfile: 'dist/index.js',
+    })
+
+    // await build({
+    //     ...sharedConfig,
+    //     platform: 'neutral',
+    //     format: 'esm',
+    //     outfile: 'dist/esm/index.js'
+    // })
+}
+
+go().then(()=>console.log("done")).catch((e)=>console.log(e))
