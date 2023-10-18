62f16c2a0e004c03eb633c3ac815e82733f8ed34
Josh Marinacci <josh@trunk.io>
2023-10-18 15:53:05 -0700

    add typescript example

---
+++ examples/import_example_ts/src/basic.ts
@@ -0,0 +1,14 @@
+import * as PImage from "pureimage";
+import * as fs from "fs";
+const img1 = PImage.make(100, 100);
+const ctx = img1.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 100, 100);
+
+PImage.encodePNGToStream(img1, fs.createWriteStream("out.png"))
+  .then(() => {
+    console.log("wrote out the png file to out.png");
+  })
+  .catch((e) => {
+    console.log("there was an error writing", e);
+  });
