55bcc8febfb33593447c23b35ba7e088e16d05a7
oligami <69239094+oligamiq@users.noreply.github.com>
2024-01-27 18:38:45 +0900

    Update browser.ts
    don't use exports

--- src/browser.ts
+++ src/browser.ts
@@ -2,9 +2,9 @@ console.log(
   "we are in the browser. No need to do anything. Just use new Canvas()",
 );
 
-exports.make = function (width, height) {
+export function make(width, height) {
   let canvas = document.createElement("canvas");
   canvas.width = width;
   canvas.height = height;
   return canvas;
-};
+}
