7d85b82e2d3e1dc19f4a1372520ba63101c472b5
joshmarinacci <joshua@marinacci.org>
2025-03-06 11:45:03 -0800

    add triangle and quadratic tests

--- test/new-rasterizer.test.ts
+++ test/new-rasterizer.test.ts
@@ -20,11 +20,53 @@ describe("draw rect 2", () => {
         }
         drawit(image1)
 
-        await save(image1, "path/rect/fill_square-fillRect-old");
+        await save(image1, "newraster/fill_square-fillRect-old");
 
         const image2 = pureimage.makeV2(10,10)
         drawit(image2)
-        await save(image2, "path/rect/fill_square-fillRect-new");
+        await save(image2, "newraster/fill_square-fillRect-new");
 
     });
+    it("fills a triangle", async () => {
+        const image1 = pureimage.make(10,10)
+        function drawit(image:pureimage.Bitmap) {
+            const c = image.getContext("2d")
+            c.fillStyle = "#0000FF";
+            c.fillRect(0, 0, 10, 10);
+            c.fillStyle = "#ff0000";
+            c.beginPath()
+            c.moveTo(1, 1)
+            c.lineTo(8, 1)
+            c.lineTo(8, 8)
+            c.lineTo(1, 1)
+            c.fill()
+        }
+        drawit(image1)
+
+        await save(image1, "newraster/fill_triangle-old");
+
+        const image2 = pureimage.makeV2(10,10)
+        drawit(image2)
+        await save(image2, "newraster/fill_triangle-new");
+    })
+    it("fills a quadratic path", async () => {
+        const image1 = pureimage.make(10,10)
+        function drawit(image:pureimage.Bitmap) {
+            const c = image.getContext("2d")
+            c.fillStyle = "#0000FF";
+            c.fillRect(0, 0, 10, 10);
+            c.fillStyle = "#ff0000";
+            c.beginPath()
+            c.moveTo(1,1)
+            c.quadraticCurveTo(15,5,1,10)
+            c.fill()
+        }
+        drawit(image1)
+
+        await save(image1, "newraster/fill_quad-old");
+
+        const image2 = pureimage.makeV2(10,10)
+        drawit(image2)
+        await save(image2, "newraster/fill_quad-new");
+    })
 })
