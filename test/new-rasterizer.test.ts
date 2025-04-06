76a9924f9fd130927f64bfebab02aa3b7b2bbf9c
joshmarinacci <joshua@marinacci.org>
2025-04-06 16:03:02 -0700

    add image comparison

--- test/new-rasterizer.test.ts
+++ test/new-rasterizer.test.ts
@@ -5,55 +5,58 @@ import { save } from "./common";
 import fs from 'fs';
 import {PNG} from 'pngjs';
 import pixelmatch from 'pixelmatch';
+import {Bitmap} from "../src/index.js";
 
-describe("draw rect 2", () => {
+type RenderTest = (image:Bitmap) => void;
+
+async function compareRenderers(test: RenderTest, pth: string) {
+    const image1 = pureimage.make(10,10)
+    test(image1)
+    await save(image1, `${pth}-old`);
+    const image2 = pureimage.makeV2(10,10)
+    test(image2)
+    await save(image2, `${pth}-new`);
+
+    const img1 = PNG.sync.read(fs.readFileSync(`output/${pth}-old.png`));
+    const img2 = PNG.sync.read(fs.readFileSync(`output/${pth}-new.png`));
+    const {width, height} = img1;
+    const diff = new PNG({width, height});
+    const match = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.0});
+    fs.writeFileSync(`output/${pth}-diff.png`, PNG.sync.write(diff));
+    expect(match).toBe(0)
+}
+
+const BLUE = 0x0000FFFF
+const RED = 0xFF0000FF
+
+describe("compare shapes", () => {
     it("fill square with fillRect()", async () => {
-        const image1 = pureimage.make(10,10)
-        function drawit(image:pureimage.Bitmap) {
+        await compareRenderers((image) => {
             const c = image.getContext("2d")
             c.fillStyle = "#0000FF";
             c.fillRect(0, 0, 10, 10);
             c.fillStyle = "#ff0000";
             c.fillRect(5, 5, 5, 5);
-            // expect(image.getPixelRGBA(0, 0)).to.eq(WHITE);
-            // expect(image.getPixelRGBA(11, 11)).to.eq(BLACK);
-            // expect(image.getPixelRGBA(50, 50)).to.eq(BLACK);
-            // expect(image.getPixelRGBA(100, 100)).to.eq(WHITE);
-        }
-        drawit(image1)
-
-        await save(image1, "newraster/fill_square-fillRect-old");
-
-        const image2 = pureimage.makeV2(10,10)
-        drawit(image2)
-        await save(image2, "newraster/fill_square-fillRect-new");
-
+            expect(image.getPixelRGBA(0, 0)).to.eq(BLUE);
+            expect(image.getPixelRGBA(6, 6)).to.eq(RED);
+        },'newraster/rect/fill_square')
     });
     it("fills a triangle", async () => {
-        const image1 = pureimage.make(10,10)
-        function drawit(image:pureimage.Bitmap) {
+        await compareRenderers((image:pureimage.Bitmap) => {
             const c = image.getContext("2d")
             c.fillStyle = "#0000FF";
             c.fillRect(0, 0, 10, 10);
             c.fillStyle = "#ff0000";
             c.beginPath()
-            c.moveTo(1, 1)
+            c.moveTo(0, 0)
             c.lineTo(8, 1)
             c.lineTo(8, 8)
             c.lineTo(1, 1)
             c.fill()
-        }
-        drawit(image1)
-
-        await save(image1, "newraster/fill_triangle-old");
-
-        const image2 = pureimage.makeV2(10,10)
-        drawit(image2)
-        await save(image2, "newraster/fill_triangle-new");
+        },'newraster/polygon/triangle')
     })
     it("fills a quadratic path", async () => {
-        const image1 = pureimage.make(10,10)
-        function drawit(image:pureimage.Bitmap) {
+        await compareRenderers((image:pureimage.Bitmap) => {
             const c = image.getContext("2d")
             c.fillStyle = "#0000FF";
             c.fillRect(0, 0, 10, 10);
@@ -62,70 +65,18 @@ describe("draw rect 2", () => {
             c.moveTo(1,1)
             c.quadraticCurveTo(15,5,1,10)
             c.fill()
-        }
-        drawit(image1)
-
-        await save(image1, "newraster/fill_quad-old");
-
-        const image2 = pureimage.makeV2(10,10)
-        drawit(image2)
-        await save(image2, "newraster/fill_quad-new");
+        },'newraster/path/quadraticCurve')
     })
     it("fills a round rect path", async () => {
-        const image1 = pureimage.make(30,30)
-        function drawit(image:pureimage.Bitmap) {
+        await compareRenderers((image:pureimage.Bitmap)=> {
             const c = image.getContext("2d")
+            c.imageSmoothingEnabled = false
             c.fillStyle = "#0000FF";
             c.fillRect(0, 0, 10, 10);
             c.fillStyle = "#ff0000";
             c.beginPath()
             c.roundRect(1, 1, 20, 20, [5, 5, 5, 5])
             c.fill()
-        }
-        drawit(image1)
-
-        await save(image1, "newraster/fill_roundrect-old");
-
-        const image2 = pureimage.makeV2(30,30)
-        drawit(image2)
-        await save(image2, "newraster/fill_roundrect-new");
-    })
-
-    it("compares images", async () => {
-        const pth = 'newraster/fill_triangle'
-        const image1 = pureimage.make(10,10)
-        function drawit(image:pureimage.Bitmap) {
-            const c = image.getContext("2d")
-            c.fillStyle = "#0000FF";
-            c.fillRect(0, 0, 10, 10);
-            c.fillStyle = "#ff0000";
-            c.beginPath()
-            c.moveTo(1, 1)
-            c.lineTo(8, 1)
-            c.lineTo(8, 8)
-            c.lineTo(1, 1)
-            c.fill()
-        }
-        drawit(image1)
-
-        await save(image1, `${pth}-old`);
-
-        const image2 = pureimage.makeV2(10,10)
-        drawit(image2)
-        await save(image2, `${pth}-new`);
-
-        function compare(pth: string) {
-            const img1 = PNG.sync.read(fs.readFileSync(`output/${pth}-old.png`));
-            const img2 = PNG.sync.read(fs.readFileSync(`output/${pth}-new.png`));
-            const {width, height} = img1;
-            const diff = new PNG({width, height});
-            const match = pixelmatch(img1.data, img2.data, diff.data, width, height, {threshold: 0.0});
-            console.log("doing the match",match)
-            fs.writeFileSync(`output/${pth}-diff.png`, PNG.sync.write(diff));
-            return match
-        }
-        const match = compare(pth)
-        console.log("doing the match",match)
-        expect(match).toBe(0)
+        },'newraster/path/roundrect')
     })
 })
